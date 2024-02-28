e60a8513aaa9de2e4ff6c12bdf75a96bd8347d73
Cristian Juarez <cjuarezr@unitru.edu.pe>
2024-02-27 19:17:28 -0500

    v0.8.9 2021-07-20

--- src/repuestos/repuestos.resolver.ts
+++ src/repuestos/repuestos.resolver.ts
@@ -9,7 +9,11 @@ import { RepuestoDto, RepuestoType } from './dto/repuesto.dto';
 export class RepuestosResolver {
   constructor(private readonly repuestoService: RepuestosService) {}
 
-  @Query((returns) => [RepuestoType], { name: 'obtener_todos_los_repuestos' })
+  @Query((returns) => [RepuestoType], {
+    name: 'obtener_todos_los_repuestos',
+    description:
+      'Esta Función retorna la información de todos los repuestos (id, producto, marca, cantidad, cantidadReserva, precio)',
+  })
   async findAll(): Promise<RepuestoType[]> {
     const repuestos = await this.repuestoService.findAll();
     return repuestos.map((repuesto) => ({
@@ -22,7 +26,11 @@ export class RepuestosResolver {
     }));
   }
 
-  @Mutation((returns) => Boolean)
+  @Mutation((returns) => Boolean, {
+    name: 'crear_repuesto',
+    description:
+      'Esta Función registra un repuesto en la base de datos y retorna true si se registro correctamente',
+  })
   async createRepuesto(
     @Args('createRepuestoInput') createRepuestoDto: CreateRepuestoDto,
   ) {
@@ -30,7 +38,11 @@ export class RepuestosResolver {
     return true;
   }
 
-  @Mutation((returns) => Boolean)
+  @Mutation((returns) => Boolean, {
+    name: 'verficar_repuestos',
+    description:
+      'Esta Función se usa internamente para actualizar repuestos en la base de datos y retorna true si se actualizo correctamente, no usar en el cliente',
+  })
   async verifyRepuesto(
     @Args('verifyRepuestoInput') verifyRepuestoDto: VerifyRepuestoDto,
   ) {
