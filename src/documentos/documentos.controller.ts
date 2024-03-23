35c56b1aeaa9e67f20dde700e3452eb02138c58f
Cristian Juarez <cjuarezr@unitru.edu.pe>
2024-03-22 23:23:08 -0500

    v1.9.91 update file corregido

--- src/documentos/documentos.controller.ts
+++ src/documentos/documentos.controller.ts
@@ -70,31 +70,45 @@ export class DocumentosController {
     @Query('query1') query1: string,
     @Query('query2') query2: string,
   ) {
-    console.log('Files:', files); // Log the files
-    console.log('Query1:', query1); // Log the query1
-    console.log('Query2:', query2); // Log the query2
-    const paths = await this.documentosService.saveFiles(files, query1, query2);
-    console.log('Paths:', paths); // Log the paths
+    try {
+      console.log('Files:', files); // Log the files
+      console.log('Query1:', query1); // Log the query1
+      console.log('Query2:', query2); // Log the query2
+      const paths = await this.documentosService.saveFiles(
+        files,
+        query1,
+        query2,
+      );
+      console.log('Paths:', paths); // Log the paths
+    } catch (error) {
+      console.error('Error subiendo archivos:', error);
+      throw error;
+    }
   }
 
   @UseGuards(JwtAuthGuard, RolesRestGuard)
   @Roles('admin', 'tecnico')
   @Get('download/*')
   async getFile(@Res() res: Response, @Param() params: any) {
-    const filePath = params[0]; // La ruta completa del archivo se almacena en params[0]
-    const path = join(process.cwd(), filePath);
-    fs.access(path, fs.constants.F_OK, (err) => {
-      if (err) {
-        console.error('El archivo no existe');
-        res.status(404).send('Archivo no encontrado');
-      } else {
-        const file = createReadStream(path);
-        file.on('error', (err) => {
-          console.error('Error al leer el archivo', err);
-          res.status(500).send('Error al leer el archivo');
-        });
-        file.pipe(res);
-      }
-    });
+    try {
+      const filePath = params[0]; // La ruta completa del archivo se almacena en params[0]
+      const path = join(process.cwd(), filePath);
+      fs.access(path, fs.constants.F_OK, (err) => {
+        if (err) {
+          console.error('El archivo no existe');
+          res.status(404).send('Archivo no encontrado');
+        } else {
+          const file = createReadStream(path);
+          file.on('error', (err) => {
+            console.error('Error al leer el archivo', err);
+            res.status(500).send('Error al leer el archivo');
+          });
+          file.pipe(res);
+        }
+      });
+    } catch (error) {
+      console.error('Error getting file:', error);
+      throw error;
+    }
   }
 }
