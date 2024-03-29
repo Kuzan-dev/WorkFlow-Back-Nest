75c67518c416d01c78d51e2032fc6cde176997a9
Cristian Juarez <cjuarezr@unitru.edu.pe>
2024-03-29 12:21:37 -0500

    corrección de historial

--- src/mantenimientos/dto/historial-admin.dto.ts
+++ src/mantenimientos/dto/historial-admin.dto.ts
@@ -2,21 +2,21 @@ import { Field, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
 export class HistorialCarData {
-  @Field(() => String)
+  @Field(() => String, { nullable: true })
   placa: string;
 
-  @Field(() => Date)
+  @Field(() => Date, { nullable: true })
   fechaSoat: Date;
 
   @Field(() => Date, { nullable: true })
   ultimaRevision: Date;
 
-  @Field(() => Date)
+  @Field(() => Date, { nullable: true })
   vigenciaContrato: Date;
 
-  @Field(() => Number)
+  @Field(() => Number, { nullable: true })
   kmActual: number;
 
-  @Field(() => Number)
+  @Field(() => Number, { nullable: true })
   operatividad: number;
 }
