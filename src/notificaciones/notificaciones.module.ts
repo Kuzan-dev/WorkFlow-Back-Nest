030978dfb528e1f736d2c3dc3a0ed590d2e75ea0
Cristian Juarez <cjuarezr@unitru.edu.pe>
2024-04-01 20:28:43 -0500

    corrección de notificaciones

--- src/notificaciones/notificaciones.module.ts
+++ src/notificaciones/notificaciones.module.ts
@@ -6,9 +6,14 @@ import {
   Notificacion,
   NotificacionSchema,
 } from './schemas/notificacion.schema';
+import { OneSignalModule } from 'onesignal-api-client-nest';
 
 @Module({
   imports: [
+    OneSignalModule.forRoot({
+      appId: '53544c67-6fbf-4324-bd8a-4f66224c9f22',
+      restApiKey: 'ZDBhYjUwMDYtMDU2ZC00NGE0LWEzOGQtNzE0MDYwMmI0ZWY2',
+    }),
     MongooseModule.forFeature([
       { name: Notificacion.name, schema: NotificacionSchema },
     ]),
--- src/notificaciones/notificaciones.service.ts
+++ src/notificaciones/notificaciones.service.ts
@@ -6,21 +6,17 @@ import {
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { pubSub } from 'src/shared/pubsub';
-import * as OneSignal from '@onesignal/node-onesignal';
+import { NotificationBySegmentBuilder } from 'onesignal-api-client-core';
+import { OneSignalService } from 'onesignal-api-client-nest';
 
 @Injectable()
 export class NotificacionesService {
-  private client: OneSignal.DefaultApi;
+  // private client: OneSignal.DefaultApi;
   constructor(
     @InjectModel(Notificacion.name)
     private notificacionModel: Model<NotificacionDocument>,
-  ) {
-    const configuration = OneSignal.createConfiguration({
-      userKey: 'ZDBhYjUwMDYtMDU2ZC00NGE0LWEzOGQtNzE0MDYwMmI0ZWY2',
-      appKey: '53544c67-6fbf-4324-bd8a-4f66224c9f22',
-    });
-    this.client = new OneSignal.DefaultApi(configuration);
-  }
+    private readonly oneSignalService: OneSignalService,
+  ) {}
 
   async crearNotificacion(
     canal: string,
@@ -42,26 +38,14 @@ export class NotificacionesService {
     await notificacion.save();
 
     pubSub.publish(canal, notificacion);
-    // Enviar la notificación a OneSignal
-    const notification = {
-      app_id: '53544c67-6fbf-4324-bd8a-4f66224c9f22',
-      contents: {
-        en: descripcion,
-      },
-      headings: {
-        en: titulo,
-      },
-      included_segments: [canal],
-    };
+    const notification = new NotificationBySegmentBuilder()
+      .setIncludedSegments([canal])
+      .notification()
+      .setContents({ en: descripcion })
+      .setHeadings({ en: titulo })
+      .build();
 
-    this.client
-      .createNotification(notification)
-      .then((response) => {
-        console.log('Notification sent successfully:', response.id);
-      })
-      .catch((e) => {
-        console.log('Error sending notification:', e);
-      });
+    await this.oneSignalService.createNotification(notification);
   }
 
   async obtenerNotificacionesNoLeidas(): Promise<Notificacion[]> {
