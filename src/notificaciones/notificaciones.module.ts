317a42bef468f2eee43e5922ed0dc81dddd540d0
Cristian Alexander Juarez Revilla <cristian@Cristians-iMac-Pro.local>
2024-04-14 10:36:31 -0500

    cambio menor para demo

--- src/notificaciones/notificaciones.module.ts
+++ src/notificaciones/notificaciones.module.ts
@@ -15,8 +15,11 @@ import { UsersModule } from 'src/users/users.module';
     AuthModule,
     UsersModule,
     OneSignalModule.forRoot({
-      appId: '53544c67-6fbf-4324-bd8a-4f66224c9f22',
-      restApiKey: 'ZDBhYjUwMDYtMDU2ZC00NGE0LWEzOGQtNzE0MDYwMmI0ZWY2',
+      // EspejoKeys
+      // appId: '53544c67-6fbf-4324-bd8a-4f66224c9f22',
+      // restApiKey: 'ZDBhYjUwMDYtMDU2ZC00NGE0LWEzOGQtNzE0MDYwMmI0ZWY2',
+      appId: '380c1226-a0dc-4f17-b4b6-c51ed58689e1',
+      restApiKey: 'Mzc0MGU2NTctNWQ5Yi00ZGI3LThjZWItNDlhYWIxNDUyZTli',
     }),
     MongooseModule.forFeature([
       { name: Notificacion.name, schema: NotificacionSchema },
