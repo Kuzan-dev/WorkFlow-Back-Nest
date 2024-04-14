ea1a8ac1dc077668cb264a2b3fc94efbf3d4bda8
Cristian Alexander Juarez Revilla <cristian@Cristians-iMac-Pro.local>
2024-04-14 10:38:10 -0500

    Espejo configuracion otra vez

--- src/notificaciones/notificaciones.module.ts
+++ src/notificaciones/notificaciones.module.ts
@@ -16,10 +16,11 @@ import { UsersModule } from 'src/users/users.module';
     UsersModule,
     OneSignalModule.forRoot({
       // EspejoKeys
-      // appId: '53544c67-6fbf-4324-bd8a-4f66224c9f22',
-      // restApiKey: 'ZDBhYjUwMDYtMDU2ZC00NGE0LWEzOGQtNzE0MDYwMmI0ZWY2',
-      appId: '380c1226-a0dc-4f17-b4b6-c51ed58689e1',
-      restApiKey: 'Mzc0MGU2NTctNWQ5Yi00ZGI3LThjZWItNDlhYWIxNDUyZTli',
+      appId: '53544c67-6fbf-4324-bd8a-4f66224c9f22',
+      restApiKey: 'ZDBhYjUwMDYtMDU2ZC00NGE0LWEzOGQtNzE0MDYwMmI0ZWY2',
+      //WorkFlow Demo
+      // appId: '380c1226-a0dc-4f17-b4b6-c51ed58689e1',
+      // restApiKey: 'Mzc0MGU2NTctNWQ5Yi00ZGI3LThjZWItNDlhYWIxNDUyZTli',
     }),
     MongooseModule.forFeature([
       { name: Notificacion.name, schema: NotificacionSchema },
